a389517eef766a6002b5cbc0b820d04cc105e5a3
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-03-05 17:42:33 +0800

    Update config

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # celeba
-kaggle datasets download -d jessicali9530/celeba-dataset
-unzip celeba-dataset.zip  # shape: (202599, 218, 178, 3)
+mkdir -p ~/.beta-vae/celeba
+kaggle datasets download -d jessicali9530/celeba-dataset -p ~/.beta-vae/celeba
+unzip ~/.beta-vae/celeba/celeba-dataset.zip -d ~/.beta-vae/celeba/dataset  # shape: (202599, 218, 178, 3)
 
 # chairs
-wget https://www.di.ens.fr/willow/research/seeing3Dchairs/data/rendered_chairs.tar
+mkdir -p ~/.beta-vae/chair
+wget -nc -P ~/.beta-vae/chair https://www.di.ens.fr/willow/research/seeing3Dchairs/data/rendered_chairs.tar
 
 # faces
 # See https://faces.dmi.unibas.ch/bfm/bfm2019.html
